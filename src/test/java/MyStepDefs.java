5851403e90ae863109b47b9e4fa4ac1235da6112
ikaraoz <i.karaoz@outlook.com>
2020-06-02 16:48:08 +0100

    modified

--- src/test/java/MyStepDefs.java
+++ src/test/java/MyStepDefs.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyStepdefs {
+public class MyStepDefs {
 
     WebDriver driver;
     WebDriverWait wait;
